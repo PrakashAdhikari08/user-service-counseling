d9318c7a53a84462fcfa0a9d5ea84eb6f5863412
Prakash <prakashadhikarijhapali@gmail.com>
2020-10-27 21:50:35 +1100

    Created Authentication Provider || Added Custom User Details class

---
+++ src/main/java/com/counseling/userservice/security/ApplicationAuthProvider.java
@@ -0,0 +1,2 @@
+package com.counseling.userservice.security;public class ApplicationAuthProvider {
+}
---
+++ src/main/java/com/counseling/userservice/security/CustomUserDetails.java
@@ -0,0 +1,2 @@
+package com.counseling.userservice.security;public interface CustomUserDetails {
+}
